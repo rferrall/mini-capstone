5c1d34dbc43e5ce5923cdbe1a4549506352688e2
rachel <ferrallr@yahoo.com>
2019-02-13 14:26:43 -0600

    adds all html and js

---
+++ public/index.js
@@ -0,0 +1,8 @@
+/* global axios */
+
+console.log("index.js loaded");
+
+axios.get("http://localhost:3000/api/products").then(function(response) {
+  var products = response.data;
+  console.log(products);
+})
